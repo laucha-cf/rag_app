9c57c834742db9e95e4a56c49012096ea88862b4
laucha-cf <lautaropc2001@gmail.com>
2024-02-22 16:35:09 -0300

    Se agrega flask al script. Se completa el README

---
+++ README.md
@@ -0,0 +1,16 @@
+### Retrieval Augmented Generation App
+
+![App Image](https://miro.medium.com/v2/resize:fit:1400/0*Ko_ihY8ecAukf2g1.png)
+
+App que permite hacer consultas mediante una API para interactuar con un LLM con el fin de generar una respuesta (sobre un documento en particular) a la pregunta brindada por el usuario.
+
+El modelo utilizado se instancia con el objetivo de brindar respuestas concretas, concisas y con la menor creatividad posible.
+
+#### Instrucciones de Ejecución:
+
+1. Crear un entorno virtual.
+2. Instalar las librerías de Python listadas en `requirements.txt`.
+3. Ejecutar el siguiente comando dentro de la carpeta con el proyecto:
+
+```bash
+python main.py
--- main.py
+++ main.py
@@ -1,33 +1,23 @@
-"""
-Ejecución principal.
-Instanciamos la VectorDB, el Modelo LLM y realizamos consultas.
-"""
-
+from flask import Flask, render_template, request, jsonify
 from langchain_openai import OpenAIEmbeddings
-from dotenv import load_dotenv
-from utils import *
+from utils import carpeta_vacia, leer_pdf, split_chunks, crear_vector_db, leer_vector_db, instanciar_modelo
 import os
+from dotenv import load_dotenv
 
-# Credencial OpenAI
+app = Flask(__name__)
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 directorio = 'data'
-path_pdf = 'data/data.pdf' #Ubicación del documento
-persist_directory = 'pi_db' # Ubicación base de datos vectorial
-
-# Declaramos el modelo de embeddings a utilizar
+path_pdf = 'data/data.pdf'
+persist_directory = 'pi_db'
 embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
 
-# Plantilla del Prompt
 prompt_template = """
 Contesta la pregunta basándote únicamente en este contexto:
-
 {contexto}
-
 ---
-La pregunta es la siguiente: {pregunta} .
-
+La pregunta es la siguiente: {pregunta}.
 La respuesta debe respetar estos puntos:
 - No debe ser inventada. Solo contenido basado en el contexto que te brindé.
 - Debe ser en una sola oración.
@@ -36,38 +26,34 @@ La respuesta debe respetar estos puntos:
 - Debe ser en tercera persona.
 """
 
-query_text = "What did Emma decided to do?"
+@app.route('/answer', methods=['POST'])
+def get_answer():
+    user_name = request.form.get('user_name')
+    question = request.form.get('question')
+    query_text = question
 
-
-if __name__ == "__main__":
-
-    if carpeta_vacia( path=directorio ):
-        pdf_document = leer_pdf( path=path_pdf )
-        document_splited = split_chunks( pdf=pdf_document )
-        vector_db = crear_vector_db( documents=document_splited,
-                                     embeddings=embeddings,
-                                     persist_directory=persist_directory )
+    if carpeta_vacia(path=directorio):
+        pdf_document = leer_pdf(path=path_pdf)
+        document_splited = split_chunks(pdf=pdf_document)
+        vector_db = crear_vector_db(documents=document_splited, embeddings=embeddings, persist_directory=persist_directory)
     else:
-        #La vector db ya existe
-        vector_db = leer_vector_db( embeddings=embeddings,
-                                    persist_directory=persist_directory )
+        vector_db = leer_vector_db(embeddings=embeddings, persist_directory=persist_directory)
 
+    results = vector_db.similarity_search_with_relevance_scores(query_text, k=3)
 
-    # En este punto ya tenemos la vector db para realizar las consultas a OpenAI
-    
-
-    # Realizamos la búsqueda de los 3 vectores más similares con sus respectivos scores
-    results = vector_db.similarity_search_with_relevance_scores( query_text, k=3 )
-
-
-    # Respondemos a la pregunta solo si la coincidencia del primer vector es 0.7 o mayor
-    if len(results)==0 or results[0][1] <= 0.7:
+    if len(results) == 0 or results[0][1] <= 0.7:
         respuesta_texto = f'No se encontraron coincidencias con respecto a la pregunta: {query_text}'
     else:
-        chat = instanciar_modelo( openai_api_key=openai_api_key )
-        contexto = "\n\n---\n\n".join( [doc.page_content.replace('\n', ' ') for doc, _score in results] )
+        chat = instanciar_modelo(openai_api_key=openai_api_key)
+        contexto = "\n\n---\n\n".join([doc.page_content.replace('\n', ' ') for doc, _score in results])
         prompt = prompt_template.format(contexto=contexto, pregunta=query_text)
-        respuesta_texto = chat.invoke( prompt ).content
+        respuesta_texto = chat.invoke(prompt).content
 
+    return jsonify({"user_name": user_name, "question": question, "answer": respuesta_texto})
 
-    print(f'La respuesta es : {respuesta_texto}')
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
